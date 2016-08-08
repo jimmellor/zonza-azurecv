298b43f22c04891ed94dbc0c57af70534b4a78f9
Jim Mellor <jim.mellor@me.com>
2016-08-08 02:08:57 +0100

    fixing char encode issue

--- azure_cv_ocr.py
+++ azure_cv_ocr.py
@@ -15,6 +15,8 @@ def get_text(data):
                 else:
                     line_text = word["text"]
             if region_text != "":
+                print region_text
+                print line_text
                 region_text = "{0}\n{1}".format(region_text, line_text)
             else:
                 region_text = line_text
