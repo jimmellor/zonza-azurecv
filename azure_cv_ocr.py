f51decd2aed64ef9bd3bfeb1dbe443eca2f38886
Jim Mellor <jim.mellor@me.com>
2016-08-08 02:24:21 +0100

    fixing char encode issue

--- azure_cv_ocr.py
+++ azure_cv_ocr.py
@@ -24,7 +24,7 @@ def get_text(data):
             text = u"{0}\n\n{1}".format(text, region_text)
         else:
             text = region_text
-    return urrlib.quote(text, safe='')
+    return urllib.quote(text, safe='')
 
 def ocr_image(source_image, az_subs_key=settings.subscription_key):
 
