68e7ea166bb18da09a277bfe690dc0e9130af862
Jim Mellor <jim.mellor@me.com>
2016-08-11 01:03:28 +0100

    implement papertrail logging

--- syslogger.py
+++ syslogger.py
@@ -20,7 +20,7 @@ f = ContextFilter()
 logger.addFilter(f)
 
 syslog = SysLogHandler(address=(settings.log_svr_addr, settings.log_svr_port))
-formatter = logging.Formatter(u'%(asctime)s %(hostname)s zonza-azurecv: %(message)s', datefmt=u'%b %d %H:%M:%S')
+formatter = logging.Formatter('%(asctime)s {} zonza-azurecv: %(message)s'.format(socket.gethostname()), datefmt=u'%b %d %H:%M:%S')
 
 syslog.setFormatter(formatter)
 logger.addHandler(syslog)
